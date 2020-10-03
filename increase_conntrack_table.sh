2b3eba4ddae349791248abcce211c6f1d0b4a4bf
congnghia0609 <congnghia0609@gmail.com>
2020-10-04 01:04:49 +0700

    update script

--- increase_conntrack_table.sh
+++ increase_conntrack_table.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo 2621440 > /proc/sys/net/netfilter/nf_conntrack_max
+# echo 2621440 > /proc/sys/net/netfilter/nf_conntrack_max
+echo 1048576 > /proc/sys/net/netfilter/nf_conntrack_max
